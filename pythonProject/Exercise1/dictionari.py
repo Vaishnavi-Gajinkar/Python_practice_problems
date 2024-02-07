3ea59510f855d7d1439bcec5dedb9be98662d494
Vaishnavi-Gajinkar <vaishnavi.gajinkar@gmail.com>
2024-02-07 16:37:43 +0530

    phone number to text code added

---
+++ pythonProject/Exercise1/dictionari.py
@@ -0,0 +1,18 @@
+number = input("Enter your phone number")
+table = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+    "10": "Ten"
+}
+output = ""
+for i in number:
+    output = output + table.get(i,"!") + " "
+
+print(output)
